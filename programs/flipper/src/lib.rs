86ca43a0c4285d827619e84a87f0651172402288
Knox Hutchinson <47797909+DataKnox@users.noreply.github.com>
2022-01-21 14:56:47 -0600

    replace with sample code

--- programs/flipper/src/lib.rs
+++ programs/flipper/src/lib.rs
@@ -1,22 +1,22 @@
 use anchor_lang::prelude::*;
 
-declare_id!("2q7fhNtQDSc4PkEwT8MU2L1GTLSWL3hLPx25ivrxcwPv");
+declare_id!("GAXBtrt9LBtRjKL6oWxSe7J1tBncGq61CcjpV7juFew8");
 
 #[program]
 pub mod flipper {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        let dataAccount = &mut ctx.accounts.dataAccount;
-        dataAccount.result = true;
+        let switchAccount = &mut ctx.accounts.switchAccount;
+        switchAccount.state = true;
         Ok(())
     }
 
     pub fn flip(ctx: Context<Flip>) -> ProgramResult {
-        let dataAccount = &mut ctx.accounts.dataAccount;
-        if dataAccount.result {
-            dataAccount.result = false
+        let switchAccount = &mut ctx.accounts.switchAccount;
+        if switchAccount.state {
+            switchAccount.state = false;
         } else {
-            dataAccount.result = true
+            switchAccount.state = true;
         }
         Ok(())
     }
@@ -25,19 +25,18 @@ pub mod flipper {
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(init, payer = user, space = 16 + 16)]
-    pub dataAccount: Account<'info, DataAccount>,
+    pub switchAccount: Account<'info, SwitchAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub system_program: Program<'info, System>
 }
 
 #[derive(Accounts)]
 pub struct Flip<'info> {
     #[account(mut)]
-    pub dataAccount: Account<'info, DataAccount>,
+    pub switchAccount: Account<'info, SwitchAccount>,
 }
-
 #[account]
-pub struct DataAccount {
-    pub result: bool,
+pub struct SwitchAccount {
+    pub state: bool,
 }
